2ea2a5409079dd163af20d17d84bd69fc4beb8e9
Kher Phay Chang <kayphay92@gmail.com>
2021-11-21 22:36:12 +0800

    add page title

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,19 +7,37 @@
 	import type { LoadInput } from '@sveltejs/kit';
 	// utils
 	import { httpGetDetails } from '$lib/utils/http.utils';
+	import routes from '$lib/constants/routes';
 	
-	export const load = async ({ fetch }: LoadInput) => {
+	const capitalizeFirstLetter = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+	const formatPageTitle = (path: string, brand: string) => {
+		if (path === routes.root) {
+			return brand;
+		}
+		const paths: string[] = path.split("/");
+		return `${brand}: ${capitalizeFirstLetter(paths[paths.length - 1])}`;
+	};
+
+	export const load = async ({ page, fetch }: LoadInput) => {
     const props: { details: AppDetails } = await httpGetDetails(fetch);
 		return {
-			props
+			props: {
+				...props,
+				pageTitle: formatPageTitle(page.path, props.details?.brand)
+			}
 		};
   }
 </script>
 
 <script lang="ts">
 	export let details: AppDetails;
+	export let pageTitle: string;
 </script>
 
+<svelte:head>
+	<title>{pageTitle}</title>
+</svelte:head>
 <Header {details} />
 <main>
   <slot />
